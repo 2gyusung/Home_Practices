028ceb02fd2c3417d74c2f68e7ae829c2bd28c11
Gyusung Lee <97gyusung@gmail.com>
2022-03-05 18:47:21 +0900

    feat : 자바스크립트 연습

---
+++ JavaScript/학교성적.js
@@ -0,0 +1,9 @@
+var score = 100;
+
+if( score > 90 ) {
+  console.log('이번 성적은 A 입니다')
+}else if( score <= 90 && score > 80) {
+  console.log('이번 성적은 B 입니다')
+} else if( score <= 80 && score > 70) {
+  console.log('이번 성적은 C 입니다')
+}
