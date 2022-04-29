88386b7ad7f22383bc5d2dab05284265590ab5a0
gyusung117 <97gyusung@gmail.com>
2022-04-29 15:55:28 +0900

    feat : 배열

---
+++ 인프런 강의 내용/array/script.js
@@ -0,0 +1,18 @@
+
+/*const odd = [1, 3, 5, 7, 9]
+const even = new Array(2, 4, 6, 8, 10)
+
+console.log(odd);
+console.log(even);*/
+
+const kor = ['서울', '대전', '부산', '제주', '여수']
+
+
+// console.log(kor[3]);
+
+let index = 2;
+
+console.log(kor[index]);
+
+kor[index] = "경주"
+console.log(kor[index]); //부산에서 경주로 바뀜
