0eca154cf2de219f77f968aeae4a3c9c647ccb07
gyusung117 <97gyusung@gmail.com>
2022-04-29 15:55:16 +0900

    fix : 로또 변경

--- 인프런 강의 내용/02_lotto/script.js
+++ 인프런 강의 내용/02_lotto/script.js
@@ -2,7 +2,8 @@
 const numbersDiv = document.querySelector('.numbers')
 const drawButton = document.querySelector('#draw')
 const resetButton = document.querySelector('#reset')
-const lottoNumbers = []
+
+const lottoNumbers = [] //배열
 const colors = ['orange', 'skyblue', 'red', 'purple', 'green']
 
 // paintNumber 함수
@@ -18,9 +19,9 @@ function paintNumber(number){
 // 추첨 버튼 클릭 이벤트 핸들링
 drawButton.addEventListener('click', function(){
   while(lottoNumbers.length < 6){
-    let ran = Math.floor(Math.random() * 45) + 1
-    if(lottoNumbers.indexOf(ran) === -1){
-      lottoNumbers.push(ran)
+    let ran = Math.floor(Math.random() * 45) + 1  //2 번 floor 소수점 제거 , 1번 random() 숫자 아무거나 출력
+    if(lottoNumbers.indexOf(ran) === -1){  //?
+      lottoNumbers.push(ran) //추가[넣는다]
       paintNumber(ran)
     }
   }
