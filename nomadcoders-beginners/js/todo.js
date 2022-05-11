877ad19325ef70a7760d8c71711fdab01a02e7bf
gyusung117 <97gyusung@gmail.com>
2022-05-11 16:13:00 +0900

    fix : todo 추가

--- nomadcoders-beginners/js/todo.js
+++ nomadcoders-beginners/js/todo.js
@@ -1,11 +1,22 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("#todo-form input")
+const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
-function handleToDoSubmit(event) {
+function paintToDo(newToDo) { //두번째
+   const list = document.createElement("li")
+   const span = document.createElement("span")
+   list.appendChild(span);
+   span.innerText = newToDo;
+   toDoList.appendChild(li); //?
+}
+
+function handleToDoSubmit(event) { //첫번째
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newToDo)
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+//#7 [2021 UPDATE] TO DO LIST 강의 다시 복습
