d2ffdb6a34d8668d90a6e4a2c8865f7c69267892
gyusung117 <97gyusung@gmail.com>
2022-05-12 12:33:07 +0900

    feat : 챌린지 복습

--- nomadcoders-beginners/submission to the challenge/src/챌린지04.js
+++ nomadcoders-beginners/submission to the challenge/src/챌린지04.js
@@ -2,12 +2,15 @@
 const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 
 const change = document.querySelector("h2");
+const test = document.querySelector("p");
 
 
 const superEventHandler = {
     handleMouseOn: function() {
         change.innerText =" The mouse is here !";
         change.style.color = colors[0];
+        test.innerText = " 연습합니다 자바스크립트"
+        test.style.color = 'blue'
     },
     handleMouseLeave: function() {
         change.innerText =" The mouse is gone !";
@@ -30,4 +33,5 @@ change.addEventListener("mouseenter",superEventHandler.handleMouseOn);
 change.addEventListener("mouseleave",superEventHandler.handleMouseLeave);
 change.addEventListener("resize",superEventHandler.handleMouseResize);
 change.addEventListener("contextmenu",superEventHandler.handleMouseRightClick);
+test.addEventListener("mouseenter",superEventHandler.handleMouseOn);
 
