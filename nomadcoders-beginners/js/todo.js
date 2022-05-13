175aceba278f41d6f6667aeda50bb0ffa2398845
gyusung117 <97gyusung@gmail.com>
2022-05-13 14:58:32 +0900

    fix : stringify , parse 내용 추가

--- nomadcoders-beginners/js/todo.js
+++ nomadcoders-beginners/js/todo.js
@@ -1,14 +1,22 @@
+const { stringify } = require("querystring");
+
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+
+const TODOS_KEY = "todos";
+
+
 const toDos = [];
 
 
-   //JSON.stringify(변수명) -> 자바스크립트의 값이나 객체(array등)를 스트링으로 변환
-    //JSON.parse("스트링") -> 단순한 스트링을 자바스크립트가 이해할 수 있는 array로 변환 
 function saveToDos() {
-    localStorage.setItem("todos",JSON.stringify(toDos));
+    //JSON.stringify(변수명) -> 자바스크립트의 값이나 객체(array등)를 스트링으로 변환
+     //JSON.parse("스트링") -> 단순한 스트링을 자바스크립트가 이해할 수 있는 array로 변환 
+                        // key : value
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos)); // 작동의미
+    //  localStorage array로 저장못함
 }
 
 function deleteToDo(event) {//세번째 todo [삭제 버튼 이벤트] 
@@ -42,7 +50,25 @@ function handleToDoSubmit(event) { //첫번째
     
 }
 
+
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+// stringify : 값을 string으로 저장 하고 싶을 때 사용
+// parse() : array로 변경
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos); //배열
+  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+}
+// ⬆ 화살표 함수랑 동일함
+// fuction sayHello(item){
+//     console.log("this is the turn of ", item);
+// }
+
+
 //#7 [2021 UPDATE] TO DO LIST 강의 전체적 으로 다시 복습
+// #7.4 Loading To Dos part One  1 :45
 // https://velog.io/@csea9000/%EB%85%B8%EB%A7%88%EB%93%9C%EC%BD%94%EB%8D%94-Vanilla-JS-TODOLIST :: 참고하기
+
+// https://oper0116.tistory.com/13 :JSON.parse , stringify 블로그
