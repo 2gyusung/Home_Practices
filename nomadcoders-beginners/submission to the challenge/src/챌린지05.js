7137f4b064e6a7996d44ee4ee3b91464e0a6afba
Gyusung Lee <97gyusung@gmail.com>
2022-05-14 19:27:26 +0900

    fix : 노마드 코더 5일차 답안 업로드

--- nomadcoders-beginners/submission to the challenge/src/챌린지05.js
+++ nomadcoders-beginners/submission to the challenge/src/챌린지05.js
@@ -15,4 +15,54 @@ function handleReSize() {
   }
 }
 
-window.addEventListener("resize", handleReSize);
+
+// 노마드 코더 코드 답 💡: 
+// window.addEventListener("resize", handleReSize);
+
+
+// const body = document.body;
+
+// const BIG_SCREEN = "bigScreen";
+// const MEDIUM_SCREEN = "mediumScreen";
+// const SMALL_SCREEN = "smallScreen";
+
+// function handleResize() {
+//   const width = window.innerWidth;
+//   if (width > 1000) {
+//     body.classList.add(BIG_SCREEN);
+//     body.classList.remove(MEDIUM_SCREEN);
+//   } else if (width <= 1140 && width >= 700) {
+//     body.classList.add(MEDIUM_SCREEN);
+//     body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
+//   } else {
+//     body.classList.remove(MEDIUM_SCREEN);
+//     body.classList.add(SMALL_SCREEN);
+//   }
+// }
+
+// window.addEventListener("resize", handleResize);
+
+/* 
+style.css
+
+body {
+  font-family: sans-serif;
+  display: flex;
+}
+
+h2 {
+  color: white;
+}
+
+.bigScreen {
+  background-color: #f1c40f;
+}
+
+.mediumScreen {
+  background-color: #9b59b6;
+}
+
+.smallScreen {
+  background-color: #3498db;
+}
+ */
