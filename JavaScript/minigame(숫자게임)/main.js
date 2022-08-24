dae8a0ccd87d52b9728212e52fe052d767db6529
Gyusung Lee <97gyusung@gmail.com>
2022-08-25 00:35:44 +0900

    fix :  업로드 및 수정

--- JavaScript/minigame(숫자게임)/main.js
+++ JavaScript/minigame(숫자게임)/main.js
@@ -20,7 +20,7 @@ function pickRandomNum() {
 // 입력 구현
 function play() {
   let userValue = userInput.value;
-  if(userValue < 1 || userValue > 10) {
+  if(userValue < 1 || userValue > 10) { // 유효성 검사
     resultArea.textContent = " 1 ~ 10 사이 숫자를 입력해 주세요!"
     return ;
   }
@@ -75,3 +75,9 @@ userInput.addEventListener('focus' , function(){
   userInput.value = ""
 })
 pickRandomNum()
+
+// 남은 기회를 5번이 아닌 10번으로 바꾸고 싶다면? 어디를 바꾸면 될까? O
+// 숫자를 맞춘후로 게임을 더이상 플레이 못하게 하기위해 버튼을 disable하는 방법은? O
+// if(gameover == true)를(46번째줄 근처) if(gameover)로 해도 똑같이 동작이 될까? 동작이 된다면 왤까? O
+// computerNum 를 1~10사이의 랜덤번호를 받고싶다면? O
+// input창에 포커스를 두면 내가 그전에 입력한 값이 자동으로 지워지게 하려면 어떻게 해야할까? (유저는 매번 입력한 값을 일일이 지우기 귀찮다)  O
